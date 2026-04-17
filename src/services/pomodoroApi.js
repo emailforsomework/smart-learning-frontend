b9ece3b908b8d84d4f40386c0c68bdb56b92b75c
emailforsomework <emailforsomework@gmail.com>
2026-04-17 14:57:54 +0530

    Update pomodoroApi.js

--- src/services/pomodoroApi.js
+++ src/services/pomodoroApi.js
@@ -2,4 +2,5 @@ import api from './api'
 
 export const startPomodoro    = (data) => api.post('/pomodoro/start', data)
 export const completePomodoro = (id)   => api.patch(`/pomodoro/${id}/complete`)
+export const togglePomodoro   = (id, data) => api.patch(`/pomodoro/${id}/toggle`, data)
 export const getActivePomodoro = ()    => api.get('/pomodoro/active')
